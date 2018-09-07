7ca3037741d9accfa4569460a3ef1fd9d85c30d4
untael <pashamironow@gmail.com>
2018-09-07 17:38:09 +0300

    add (model): Create Contact model and actions

--- app.js
+++ app.js
@@ -4,12 +4,21 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const morgan = require('morgan')
 const cors = require('cors')
+require('./models/contact.model')
+const Contact = mongoose.model('contacts')
 
 // =======================
 // configuration =========
 // =======================
+
+// connect to database
+mongoose.connect('mongodb://localhost/contacts')
+  .then(() => console.log('MongoDB has started ...'))
+  .catch(e => console.log(e))
+
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -18,6 +27,7 @@ app.use(cors())
 
 // use morgan to log requests to the console
 app.use(morgan('dev'))
+
 
 // =======================
 // routes ================
@@ -45,6 +55,63 @@ app.get('/display', function (req, res) {
     about: 'Programming 99 years',
   }
   console.log(contact)
+  res.send(contact)
+})
+
+// list of contacts
+app.get('/list', function (req, res) {
+  Contact.find({})
+    .then(contacts => {
+      // Presenter
+      const contactsToPresent = contacts.map(contact => {
+        return {
+          id: contact._id,
+          name: contact.name,
+          surname: contact.surname,
+          middlename: contact.middlename,
+          birthday: contact.birthday,
+          gender: contact.gender,
+          family: contact.family,
+          country: contact.country,
+          city: contact.city,
+          zip: contact.zip,
+          address: contact.address,
+          email: contact.email,
+          website: contact.website,
+          job: contact.job,
+          about: contact.about,
+        }
+      })
+      // const json = JSON.stringify(contactsToPresent)
+      res.send(contactsToPresent)
+      console.log(contactsToPresent)
+    })
+})
+
+
+// create contact
+app.post('/create', function (req, res) {
+  const contact = new Contact({
+    name: req.body.contact.name,
+    surname: req.body.contact.surname,
+    middlename: req.body.contact.middlename,
+    birthday: req.body.contact.birthday,
+    gender: req.body.contact.gender,
+    family: req.body.contact.family,
+    country: req.body.contact.country,
+    city: req.body.contact.city,
+    zip: req.body.contact.zip,
+    address: req.body.contact.address,
+    email: req.body.contact.email,
+    website: req.body.contact.website,
+    job: req.body.contact.job,
+    about: req.body.contact.about,
+  })
+  contact.save()
+    .then(contact => {
+      console.log(contact)
+    })
+    .catch(e => console.log(e))
   res.send(contact)
 })
 
---
+++ models/contact.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let contactSchema = new Schema({
+  id: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  surname: {
+    type: String,
+  },
+  middlename: {
+    type: String,
+  },
+  birthday: {
+    type: String,
+  },
+  gender: {
+    type: String,
+    default: false,
+  },
+  family: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  zip: {
+    type: Number,
+  },
+  address: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  job: {
+    type: String,
+  },
+  about: {
+    type: String,
+  },
+  phones: {
+    type: [Number],
+  },
+})
+
+mongoose.model('contacts', contactSchema)
